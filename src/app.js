7038260c43ff5e8897426b074b998f181ffc7d1e
marilynpb <g.marilyn182@hotmail.com>
2024-02-02 19:40:13 -0300

    Add validateToken

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
 import authRoutes from './routes/auth.routes.js';
 
 const app = express();
@@ -7,6 +9,8 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use('/api', authRoutes);
 
